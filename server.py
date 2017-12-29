6beada23faf5d7a10458ea9d8520a7256ab88157
clacktronics <ben@clacktronics.co.uk>
2017-12-29 08:40:51 +0000

    Working on timers and HTML for server

--- server.py
+++ server.py
@@ -1,8 +1,58 @@
 from socket import *
 from time import sleep
+from threading import Timer
+import SimpleHTTPServer
+import SocketServer
 
 s=socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+class perpetualTimer():
+
+   def __init__(self,t,hFunction):
+	  self.t=t
+	  self.hFunction = hFunction
+	  self.thread = Timer(self.t,self.handle_function)
+
+   def handle_function(self):
+	  self.hFunction()
+	  self.thread = Timer(self.t,self.handle_function)
+	  self.thread.start()
+
+   def start(self):
+	  self.thread.start()
+
+   def cancel(self):
+	  self.thread.cancel()
+
+
+PORT = 1516
+
+class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			query = [self.path.index('?')+1,self.path.index('=')]
+			cmd =  self.path[query[0]:query[1]]
+		except:
+			cmd = ''
+
+		print cmd
+
+		if cmd == "stop":
+			st.cancel()
+			print cmd
+		elif cmd == "start":
+			st.start()
+			print cmd
+		elif cmd == "step":
+			sendStep()
+			print cmd
+
+		self.path = '/control_panel.html'
+		return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+httpd = SocketServer.TCPServer(("", PORT), MyRequestHandler)
+
 
 i = 0
 
@@ -15,11 +65,24 @@ cmd = ["step",
 "ints",
 "exts"]
 
-while True:
-	if i < len(cmd)-1 : i += 1
-	else: i = 0
-
-	message = cmd[i]
+def sendStep():
+	message = cmd[0]
 	s.sendto(message,('',1515))
-	print message
-	sleep(0.5)
+	print "sent" + message
+
+st = perpetualTimer(1,sendStep)
+st.start()
+
+def main():
+	print "Serving at port", PORT
+	httpd.serve_forever()
+
+try:
+	main()
+except (KeyboardInterrupt, SystemExit):
+	st.cancel()
+	httpd.shutdown()
+except Exception as e:
+	print e
+	st.cancel()
+	httpd.shutdown()
